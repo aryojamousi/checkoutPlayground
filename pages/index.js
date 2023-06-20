46f2f7bcc957eca61fe98244031b01ae3f061dff
Aryo <aryo.jamousi@thomann.io>
2023-06-20 15:43:00 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,74 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import xml2js from 'xml2js';
+import { useState } from 'react';
+
+function compare( a, b ) {
+  if (!a.artname) {
+    return -1
+  }
+  if (!b.artname) {
+    return -1;
+  }
+  if ( a.artname[0] < b.artname[0] ){
+    return -1;
+  }
+  if ( a.artname[0] > b.artname[0] ){
+    return 1;
+  }
+  return 0;
+}
+
+// Path to the XML file
+function sortXML(xml) {
+  // Parse the XML data
+  const parser = new xml2js.Parser();
+  var sorted = null;
+  parser.parseString(xml, (err, result) => {
+    if (err) {
+      console.error('Error parsing XML:', err);
+      return;
+    }
+
+    result.message.item.sort(compare);
+    result.message.item.forEach((element, index) => {
+      result.message.item[index] = Object.keys(result.message.item[index]).sort().reduce(
+        (obj, key) => { 
+          obj[key] = result.message.item[index][key]; 
+          return obj;
+        }, 
+        {}
+      );
+    });
+
+    // Convert the result back to XML
+    const builder = new xml2js.Builder();
+    const reversedXmlData = builder.buildObject(result);
+    sorted = reversedXmlData;
+  });
+  return sorted;
+};
 
 export default function Home() {
+  const [sortedValue, setSortedValue] = useState('');
+  
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main style={{ display: 'flex'}}>
+        <div className={styles.xml}>
+          Input Xml 
+          <textarea style={{ width: 200, height: 500 }} id="xmlText" />
+          <button onClick={() => setSortedValue(sortXML(document.getElementById('xmlText').value))} >Sort</button>
+        </div>
+        <div className={styles.xml}>
+          Sorted Xml 
+          <textarea value={sortedValue} readOnly style={{ width: 200, height: 500 }} id="sortedXmlText"/>
+          <button onClick={() => {
+            setSortedValue(sortXML(document.getElementById('xmlText').value));
+            navigator.clipboard.writeText(sortXML(document.getElementById('xmlText').value))
+          }}>Sort and copy to clipboard</button>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
